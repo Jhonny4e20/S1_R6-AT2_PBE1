86bd431e95243401fb04ea8378b4cdbaacd6510a
unknown <pedrao.kovalski@gmail.com>
2025-11-18 13:07:35 -0300

    S1_R6-AT2_PBE1

---
+++ app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const {clienteRoutes} = require("./src/routes/clienteRoutes");
+const {pedidoRoutes} = require("./src/routes/pedidoRoutes");
+
+const PORT = 8081;
+
+app.use(express.json());
+
+app.use('/', clienteRoutes);
+app.use('/', pedidoRoutes);
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
